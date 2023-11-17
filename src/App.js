be06f3ef5a152736f4fc341ef2e15c655c5ca839
serenitysmith <serenitysmith24@gmail.com>
2023-11-17 01:03:13 -0500

    children practice react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
 
+// basically just cleaned up the code and called it all form one oage wiht props and callout components 
+
+import React from "react";
+// import InfoCallout from "./InfoCallout";
+// import ImageCallout from "./ImageCallout";
+// import EmailCallout from "./EmailCallout";
+import "./App.css";
+
+// imported callout
+import Callout from "./Callout";
+
+// for the practice look at callout.js page, thats where we did the work to make children so everything wasnt so sepereated.
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Welcome!</h1>
+
+      {/* 
+      //take info from infocall out and put it below in props */}
+
+<Callout>
+                <h2>Don't miss out!</h2>
+                <p>Unless you don't suffer from FOMO, you better make sure you fill out the email form below!</p>
+            </Callout>
+
+
+
+      {/* <InfoCallout
+        header="Don't miss out!"
+        body="Unless you don't suffer from FOMO, you better make sure you fill out the email form below!"
+      /> */}
+
+      <p>
+        This is probably the best site you've ever come across. I'm glad you're
+        here to witness the magnificence of this website right now.
+      </p>
+
+      {/* <ImageCallout
+        img={"https://picsum.photos/id/102/4320/3240"}
+        caption="Just look at those sparkling raspberries!"
+      /> */}
+     <Callout>
+                <img alt="" src="https://picsum.photos/id/102/4320/3240" width="100%" />
+                <figcaption>Just look at those sparkling raspberries!</figcaption>
+            </Callout>
+      <p>
+        Here's some more unforgettable content. Lorem ipsum something or other.
+      </p>
+      <Callout>
+                <h2>Give us your email. We definitely won't sell it to anyone.</h2>
+                <input type="email" placeholder="Enter Email"/>
+                <button>Sign me up!</button>
+            </Callout>
+            
+    </main>
   );
 }
 
